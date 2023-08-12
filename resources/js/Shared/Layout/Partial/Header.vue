cf209ad4b48234d5c3061d86bc5387042a0d0eee
Rachmad Nur Hayat <44340813+rachyharkov@users.noreply.github.com>
2023-08-12 16:23:22 +0700

    fix error on build

--- resources/js/Shared/Layout/Partial/Header.vue
+++ resources/js/Shared/Layout/Partial/Header.vue
@@ -3,6 +3,7 @@
     import { Link } from '@inertiajs/vue3';
     import dynamicEventBus from '@/utils/helper/dynamicEventBus.js';
     import { syncProgress } from '@/utils/helper/syncProgress.js';
+    import FotoProfil from '/public/assets/static/images/faces/1.jpg'
 
     export default {
         data() {
@@ -85,7 +86,7 @@
                             </div>
                             <div class="user-img d-flex align-items-center">
                                 <div class="avatar avatar-md">
-                                    <img src="/assets/static/images/faces/1.jpg">
+                                    <img :src="FotoProfil">
                                 </div>
                                 <div v-if="is_syncing" style="width: 17px; height: 17px; right: 5px; bottom: -1px" class="bg-warning rounded-circle position-absolute text-primary z-index-1 anim-rotate">
                                     <i class="bi bi-arrow-repeat text-white position-relative" style="font-size: 0.8rem; margin-left: 0.14rem; top: -0.36rem;"></i>
